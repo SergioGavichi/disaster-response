bb579fba322b3390ab221007c862d91c8c98d5e5
Stephane Garcia <garcia.stef@gmail.com>
2019-09-14 15:29:24 +0200

    modify GridCV parameters

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -68,8 +68,8 @@ def build_model():
     ])
 
     parameters = {
-        'vect__ngram_range': ((1, 1), (1, 2)),
-        'vect__max_df': (0.5, 0.75, 1.0),
+        #'vect__ngram_range': ((1, 1), (1, 2)),
+        'vect__max_df': (0.5, 0.75),
         'vect__max_features': (None, 5000),
         'tfidf__use_idf': (True, False),
         'clf__estimator__n_estimators': [50, 100, 200],
